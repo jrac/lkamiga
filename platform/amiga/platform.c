48fbea311c251971f14ebe2ae6996785a023d789
Josh Cummings <josh@joshcummings.net>
2026-01-27 13:18:54 +1100

    Add license/copyright header to amiga platform code

--- platform/amiga/platform.c
+++ platform/amiga/platform.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025-2026 Josh Cummings
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT
+ */
+
 #include <kernel/debug.h>
 #include <kernel/thread.h>
 #include <lib/console.h>
--- platform/amiga/serial.c
+++ platform/amiga/serial.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025-2026 Josh Cummings
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT
+ */
+
 #include "platform_p.h"
 
 #include <kernel/event.h>
